5e0e84b1774cb1ec87e08e2924b24b83e6837026
almslsbest <185805234@qq.com>
2024-04-26 16:05:02 +0800

    init

---
+++ .vitepress/config.mjs
@@ -0,0 +1,64 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/logo_dingding.png" }]],
+  title: "学习文档项目",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', items : [
+          {text:"首页" ,link:"/"}
+        ] },
+      { text: 'Examples', link: '/markdown-examples' },
+      {text :"java" , items :[
+          {text: "基础语法" ,link: '/markdown-examples'}
+        ]},
+      {text:"两侧栏样式",link:'/two_side_example'}
+    ],
+
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+
+    sidebar :true,
+    aside :"left",
+
+
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/almsIsbest' }
+    ],
+    footer:{
+      copyright : "CopyRight@ 2024 Alms"
+    },
+    // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    }
+
+  }
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "我的文档项目"
+  text: "一个文档站点"
+  tagline: 每一次挥拳不仅是对对手的抗争，也是对自己极限的挑战。
+  image:
+    src : /study.png
+    alt: 背景图片
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: /markdown-examples
+    - theme: alt
+      text: API 实例
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: 特征1的详细描述
+  - title: Feature B
+    details: 特征2的详细描述
+  - title: Feature C
+    details: 特征3的详细描述
+---
+
